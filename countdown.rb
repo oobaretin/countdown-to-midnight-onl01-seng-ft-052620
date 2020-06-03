ecc488e3e8ab4197c2fd62b002913d25e59d5e3c
osagie obaretin <osagie.codes@gmail.com>
2020-06-03 05:12:23 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,12 +1,8 @@
-
-
-def countdown
-  def countdown(x)
-while x > 0
-  puts "#{x} SECOND(S)!"
-  sleep 1
-  x -= 1
-end
-return "HAPPY NEW YEAR!"
-end
+ def countdown(x)
+  while x > 0
+   puts "#{x} SECOND(S)!"
+   sleep 1
+   x -= 1
+ end
+ return "HAPPY NEW YEAR!"
 end
